85c3022995ce38fe3a409854ca249e305f5769f1
Broderick Arneson <broderic@cs.ualberta.ca>
2009-04-22 21:59:11 -0600

    MoHex: set knowledge threshold to 100 by default.

--- src/uct/HexUctSearch.cpp
+++ src/uct/HexUctSearch.cpp
@@ -67,6 +67,7 @@ HexUctSearch::HexUctSearch(SgUctThreadStateFactory* factory,
 {
     SetBiasTermConstant(0.0);
     SetExpandThreshold(1);
+    SetKnowledgeThreshold(100);
     SetMaxNodes(15000000);
     SetMoveSelect(SG_UCTMOVESELECT_COUNT);
     SetNumberThreads(1);    
