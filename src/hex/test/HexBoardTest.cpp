d4ead9d5f1b7e9060f583570a2b6699a90e75fa2
Broderick Arneson <broderic@cs.ualberta.ca>
2009-05-17 17:49:23 -0600

    Fix compile error.

--- src/hex/test/HexBoardTest.cpp
+++ src/hex/test/HexBoardTest.cpp
@@ -23,7 +23,7 @@ BOOST_AUTO_TEST_CASE(HexBoard_PlayAndUndo)
     HexBoard brd(7, 7, ice, param);
 
     brd.startNewGame();
-    brd.ComputeAll(BLACK, HexBoard::DO_NOT_REMOVE_WINNING_FILLIN);
+    brd.ComputeAll(BLACK);
     BOOST_CHECK(!brd.Cons(BLACK).Exists(NORTH, HEX_CELL_A4, VC::FULL));
 
     brd.PlayMove(BLACK, HEX_CELL_B2);
@@ -42,7 +42,7 @@ BOOST_AUTO_TEST_CASE(HexBoard_CopyConstructor)
     HexBoard brd(7, 7, ice, param);
     
     brd.startNewGame();
-    brd.ComputeAll(BLACK, HexBoard::DO_NOT_REMOVE_WINNING_FILLIN);
+    brd.ComputeAll(BLACK);
     brd.PlayMove(BLACK, HEX_CELL_B2);
     BOOST_CHECK_EQUAL(brd.getColor(HEX_CELL_B2), BLACK);
     BOOST_CHECK(brd.Cons(BLACK).Exists(NORTH, HEX_CELL_A4, VC::FULL));
