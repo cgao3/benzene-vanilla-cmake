4ff1ca140f24710f2bd8a88b850a2ba48f842ddc
Broderick Arneson <broderic@cs.ualberta.ca>
2009-07-24 13:19:28 -0600

    ZobristHashTest: fix compile.

--- src/hex/test/ZobristHashTest.cpp
+++ src/hex/test/ZobristHashTest.cpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------
-/** @file
+/** @file ZobristHashTest.cpp
  */
 //---------------------------------------------------------------------------
 
@@ -15,70 +15,71 @@ namespace {
 
 BOOST_AUTO_TEST_CASE(ZobristHash_InitializationAndUpdates)
 {
-    bitset_t black, white;
-    hash_t h1, h2, h3;
-    ZobristHash zh1, zh2;
-    
     // Note: due to the probabilistic nature of Zobrist
     // hashing, it is possible that some of these tests
     // can fail. However, it should be extremely improbable,
-    // to the point of never occuring in practice
+    // to the point of never occuring in practice.
+    // This is true only if USE_PREDEFINED_HASHES is false. 
+
     
-    // check that base values do NOT  differ
-    zh1 = ZobristHash();
-    zh2 = ZobristHash();
-    BOOST_CHECK(zh1.hash() == zh2.hash());
+    // check that base values do NOT differ
+    ZobristHash zh1(5, 5);
+    ZobristHash zh2(5, 5);
+    BOOST_CHECK(zh1.Hash() == zh2.Hash());
     
     // check that updates change hash value, reset restores
-    h1 = zh1.hash();
-    zh1.update(BLACK, FIRST_CELL);
-    h2 = zh1.hash();
+    hash_t h1, h2, h3;
+    h1 = zh1.Hash();
+    zh1.Update(BLACK, FIRST_CELL);
+    h2 = zh1.Hash();
     BOOST_CHECK(h1 != h2);
-    zh1.reset();
-    BOOST_CHECK_EQUAL(h1, zh1.hash());
-    zh1.update(WHITE, FIRST_CELL);
-    h3 = zh1.hash();
+    zh1.Reset();
+    BOOST_CHECK_EQUAL(h1, zh1.Hash());
+    zh1.Update(WHITE, FIRST_CELL);
+    h3 = zh1.Hash();
     BOOST_CHECK(h1 != h3);
     BOOST_CHECK(h2 != h3);
-    zh1.update(WHITE, FIRST_CELL);
-    BOOST_CHECK_EQUAL(h1, zh1.hash());
-    zh1.update(BLACK, FIRST_CELL);
-    zh1.update(WHITE, FIRST_CELL);
-    BOOST_CHECK(h1 != zh1.hash());
-    BOOST_CHECK(h2 != zh1.hash());
-    BOOST_CHECK(h3 != zh1.hash());
-    zh1.update(BLACK, FIRST_CELL);
-    BOOST_CHECK_EQUAL(h3, zh1.hash());
+    zh1.Update(WHITE, FIRST_CELL);
+    BOOST_CHECK_EQUAL(h1, zh1.Hash());
+    zh1.Update(BLACK, FIRST_CELL);
+    zh1.Update(WHITE, FIRST_CELL);
+    BOOST_CHECK(h1 != zh1.Hash());
+    BOOST_CHECK(h2 != zh1.Hash());
+    BOOST_CHECK(h3 != zh1.Hash());
+    zh1.Update(BLACK, FIRST_CELL);
+    BOOST_CHECK_EQUAL(h3, zh1.Hash());
     
     // check that sequence of updates after reset
     // obtains same result as initialization
+    bitset_t black, white;
     BOOST_REQUIRE(FIRST_CELL < FIRST_INVALID - 1);
     black.set(FIRST_CELL);
     black.set(FIRST_INVALID - 1);
     white.set(SWAP_PIECES);
-    zh1 = ZobristHash(black, white);
-    h1 = zh1.hash();
-    zh1.reset();
-    zh1.update(BLACK, FIRST_CELL);
-    BOOST_CHECK(h1 != zh1.hash());
-    zh1.update(WHITE, SWAP_PIECES);
-    BOOST_CHECK(h1 != zh1.hash());
-    zh1.update(BLACK, static_cast<HexPoint>(FIRST_INVALID - 1));
-    BOOST_CHECK_EQUAL(h1, zh1.hash());
+    zh1 = ZobristHash(5, 5);
+    zh1.Compute(black, white);
+    h1 = zh1.Hash();
+    zh1.Reset();
+    zh1.Update(BLACK, FIRST_CELL);
+    BOOST_CHECK(h1 != zh1.Hash());
+    zh1.Update(WHITE, SWAP_PIECES);
+    BOOST_CHECK(h1 != zh1.Hash());
+    zh1.Update(BLACK, static_cast<HexPoint>(FIRST_INVALID - 1));
+    BOOST_CHECK_EQUAL(h1, zh1.Hash());
     
     // check that compute gives same result as
-    // update and initialization
-    zh1.reset();
-    zh1.compute(black, white);
-    BOOST_CHECK_EQUAL(h1, zh1.hash());
-    zh1.reset();
+    // Update and initialization
+    zh1.Reset();
+    zh1.Compute(black, white);
+    BOOST_CHECK_EQUAL(h1, zh1.Hash());
+    zh1.Reset();
     for (int i=0; i<FIRST_INVALID; ++i) {
         if (black.test(i)) 
-            zh1.update(BLACK, static_cast<HexPoint>(i));
+            zh1.Update(BLACK, static_cast<HexPoint>(i));
         if (white.test(i))
-            zh1.update(WHITE, static_cast<HexPoint>(i));
+            zh1.Update(WHITE, static_cast<HexPoint>(i));
     }
-    BOOST_CHECK_EQUAL(h1, zh1.hash());
+    BOOST_CHECK_EQUAL(h1, zh1.Hash());
 }
 
 }
