b294a7748d810e92bc0fe00e3730800745f07dc7
Broderick Arneson <broderic@cs.ualberta.ca>
2010-01-08 18:23:47 -0700

    DfpnSolver uses 2m entries in hash table by default.

--- src/player/BenzeneHtpEngine.cpp
+++ src/player/BenzeneHtpEngine.cpp
@@ -33,7 +33,7 @@ BenzeneHtpEngine::BenzeneHtpEngine(GtpInputStream& in, GtpOutputStream& out,
       m_dfsSolver(),
       m_dfpnSolver(),
       m_dfsHashTable(new DfsHashTable(20)), // TT with 2^20 entries
-      m_dfpnHashTable(new DfpnHashTable(20)), // TT with 2^20 entries
+      m_dfpnHashTable(new DfpnHashTable(21)), // TT with 2^21 entries
       m_dfsDB(0),
       m_dfpnDB(0),
       m_dfsParam(),
