189b2fd899b73efaeac5c1d8222291a308387c1b
Broderick Arneson <broderic@cs.ualberta.ca>
2010-11-08 17:11:27 -0700

    Update benzene-daily script.

--- tools/cronjob/benzene-daily.sh
+++ tools/cronjob/benzene-daily.sh
@@ -13,18 +13,17 @@
 
 TEST_DIR=/local/scratch/broderic/benzene-daily
 GIT_BACKUP_DIR=~broderic/hex/benzene.git.sf.backup/
-#REPORT_EMAIL="hexml@lists.cs.ualberta.ca"
-REPORT_EMAIL="broderic@cs.ualberta.ca"
-
+REPORT_EMAIL="hexml@lists.cs.ualberta.ca"
 WEBPAGE=~broderic/web_docs/hex/local
 RESULT_TABLE=$WEBPAGE/autotest-result.dat
 RESULT_TABLE_LOCAL=$TEST_DIR/result.dat
+MAIL_EXEC=`which mail`
 
 mail-error() {
   FAILURE="$1"
   OUTPUT="$2"
   DATE=$(date)
-  /bin/mail -s "Benzene Error Report" "$REPORT_EMAIL" <<EOF
+  cat <<EOF > $TEST_DIR/last_error
 ----------------------------------------------------------------------------
 Benzene Error Report
 ----------------------------------------------------------------------------
@@ -42,6 +41,7 @@ Output:
 
 $OUTPUT
 EOF
+  $MAIL_EXEC -s "Benzene Error Report" "$REPORT_EMAIL" < $TEST_DIR/last_error
   echo -e "$(date +'%F')\t$FAILURE" >> $RESULT_TABLE_LOCAL
   cp $RESULT_TABLE_LOCAL $RESULT_TABLE
   echo "Failure: $FAILURE"
@@ -54,44 +54,77 @@ run-checked() {
   OUTPUT=$($CMD 2>&1) || mail-error "$TITLE ($CMD)" "$OUTPUT"
 }
 
-# Fuego trunk
+run-function()
+{
+    FUNC=$1
+    echo "**** Running '$FUNC' ****"
+    OUT=$($FUNC 2>&1)
+    RET=$?
+    if [ $RET == 0 ]; then
+        echo "GOOD"
+        return 0
+    elif [ $RET == 1 ]; then
+        exit 1
+    fi
+    echo "ERROR"
+    mail-error "Error in $FUNC()" "$OUT"
+}
 
-cd $TEST_DIR || exit 1
-rm -rf fuego
-svn co https://fuego.svn.sourceforge.net/svnroot/fuego/trunk fuego || exit 1
-cd fuego || exit 1
-autoreconf -i
-./configure --enable-assert
-run-checked "make" "FUEGO COMPILATION"
+# Each of these functions is called by run-function() as a command.
+# If the run fails a run-checked(), run-checked() will mail the error
+# and abort with error 1. If there is some other error in the
+# function that is not inside a run-checked(), the function should
+# abort with error code 2.  run-function() will report error code 2s with
+# mail-error() and stop the testing.
+build-fuego()
+{
+    PATH=$TEST_DIR:/usr/local/bin:$PATH
+    cd $TEST_DIR || return 2
+    echo "Checking for gogui-regress..."
+    test -x ./gogui-regress || return 2
+    test -f ./gogui-regress.jar || return 2
+    echo "Found!"
 
-# Backup the benzene repository
-rsync -av benzene.git.sourceforge.net::gitroot/benzene/* $GIT_BACKUP_DIR
+    cd $TEST_DIR || return 2
+    rm -rf fuego
+    svn -q co https://fuego.svn.sourceforge.net/svnroot/fuego/trunk fuego || return 2
+    cd fuego || return 2
+    autoreconf -i || return 2
+    run-checked "./configure --enable-assert" "FUEGO CONFIGURE"
+    run-checked "make -s" "FUEGO COMPILATION"
+    return 0
+}
 
-# Clone and build benzene
+build-benzene()
+{
+    # Backup the benzene repository
+    rsync -av benzene.git.sourceforge.net::gitroot/benzene/* $GIT_BACKUP_DIR
 
-cd $TEST_DIR || exit 1
-rm -rf benzene
-git clone git://benzene.git.sourceforge.net/gitroot/benzene/benzene benzene || exit 1
-cd benzene || exit 1
-autoreconf -i
-./configure --enable-assert --with-fuego-root=$TEST_DIR/fuego
-run-checked "make" "BENZENE COMPILATION"
-run-checked "make check" "BENZENE MAKE CHECK"
+    # Clone and build benzene
+    cd $TEST_DIR || return 2
+    rm -rf benzene
+    git clone git://benzene.git.sourceforge.net/gitroot/benzene/benzene benzene || return 2
+    cd benzene || return 2
+    autoreconf -i
+    run-checked "./configure --enable-assert --with-fuego-root=$TEST_DIR/fuego" "BENZENE CONFIGURE"
+    run-checked "make -s" "BENZENE COMPILATION"
+    run-checked "make -s check" "BENZENE MAKE CHECK"
+    cd $TEST_DIR/benzene/regression || return 2
+    run-checked "./run.sh" "BENZENE BASIC REGRESSION TESTS"
 
-# Run unit and regresion tests
-run-checked "src/test/benzene_unittest" "BENZENE UNIT TESTS"
+    # Build documentation and upload to server
+    cd $TEST_DIR/benzene/doc || return 2
+    make -s
+    tar czf benzene-doc.tar.gz benzene-doc/
+    scp benzene-doc.tar.gz $WEBPAGE
+    cd $WEBPAGE && rm -rf benzene-doc && tar xzf benzene-doc.tar.gz && chmod a+rX -R benzene-doc
+}
 
-cd $TEST_DIR/benzene/regression
-run-checked "./run.sh" "BENZENE BASIC REGRESSION TESTS"
-
-cd $TEST_DIR/benzene/doc
-make -s
-
-# Build documentation and upload to server
-tar czf benzene-doc.tar.gz benzene-doc/
-scp benzene-doc.tar.gz $WEBPAGE
-cd $WEBPAGE && rm -rf benzene-doc && tar xzf benzene-doc.tar.gz && chmod a+rX -R benzene-doc
+rm -f $TEST_DIR/last_error
+run-function "build-fuego" \
+    && run-function "build-benzene"
 
 echo -e "$(date +'%F')\tSUCCESS" >> $RESULT_TABLE_LOCAL
 cp $RESULT_TABLE_LOCAL $RESULT_TABLE
 echo "Success"
+
