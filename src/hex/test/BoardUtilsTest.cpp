d757405189d19788aba9c0339b21788eba3330b2
Broderick Arneson <broderic@cs.ualberta.ca>
2011-01-08 19:47:24 -0700

    Fix commit.

--- src/hex/test/BoardUtilsTest.cpp
+++ src/hex/test/BoardUtilsTest.cpp
@@ -219,53 +219,6 @@ BOOST_AUTO_TEST_CASE(BoardUtil_RandomEmptyCell)
     BOOST_CHECK_EQUAL(p, HEX_CELL_A1);
 }
 
-BOOST_AUTO_TEST_CASE(BoardUtil_Decompositions)
-{
-    ICEngine ice;
-    VCBuilderParam param;
-    HexBoard brd(7, 7, ice, param);
-
-    std::string str(". . . . W B ."
-                     ". . . . . . ."
-                      ". B B B W . ."
-                       ". B B W . . ."
-                        ". . W . . . ."
-                         ". . W . . . ."
-                          ". . . . . . .");
-    brd.GetPosition().SetPosition(str);
-
-    // Find decomp between E1, B3, WEST, and NORTH. 
-    brd.SetUseDecompositions(false);
-    brd.ComputeAll(BLACK);
-    brd.SetUseDecompositions(true);
-    bitset_t capturedVC;
-    BOOST_CHECK(BoardUtils::FindCombinatorialDecomposition(brd, BLACK, 
-                                                           capturedVC));
-    BOOST_CHECK(capturedVC.any());
-}
-
-BOOST_AUTO_TEST_CASE(BoardUtil_SplitDecompositions)
-{
-    ICEngine ice;
-    VCBuilderParam param;
-    HexBoard brd(7, 7, ice, param);
-    
-    std::string s(". . . . W B ."
-                   ". . . . . . ."
-                    ". B B B W . ."
-                     ". B B W . . ."
-                      ". . W . . . ."
-                       ". . W . . . ."
-                        ". . . . . . .");
-    brd.GetPosition().SetPosition(s);
-
-    // Find splitting decomp between NORTH, E3, SOUTH.
-    brd.ComputeAll(WHITE);
-    HexPoint group;
-    BOOST_CHECK(BoardUtils::FindSplittingDecomposition(brd, WHITE, group));
-    BOOST_CHECK_EQUAL(group, HEX_CELL_E3);
-}
-
 } // namespace
 
 //---------------------------------------------------------------------------
