a0a9bd69ba745a6a96b6da4f9540f96b78e4b04f
Broderick Arneson <broderic@cs.ualberta.ca>
2011-01-21 17:33:09 -0700

    Move WolveSearch into its own file.

--- src/wolve/WolvePlayer.cpp
+++ src/wolve/WolvePlayer.cpp
@@ -17,22 +17,8 @@ using namespace benzene;
 
 //----------------------------------------------------------------------------
 
-namespace {
-
-static const int sgTopScore = SgSearchValue::MIN_PROVEN_VALUE - 1;
-static const int sgBottomScore = -sgTopScore;
-static const HexEval topScore = +10.0;
-static const HexEval bottomScore = -10.0;
-static const HexEval factor = (sgTopScore - sgBottomScore)
-    / (topScore - bottomScore);
-
-/** Converts a floating point score to a score used in SgSearch. */
-int ConvertToSgScore(HexEval score)
+namespace 
 {
-    score = std::max(score, bottomScore);
-    score = std::min(score, topScore);
-    return static_cast<int>(score * factor);
-}
 
 std::string PrintSgScore(int score)
 {
@@ -54,6 +40,7 @@ std::string PrintVector(const SgVector<SgMove>& vec)
 }
 
 }
+
 //----------------------------------------------------------------------------
 
 WolvePlayer::WolvePlayer()
@@ -116,158 +103,3 @@ std::string WolvePlayer::PrintStatistics(int score, const SgVector<SgMove>& pv)
 
 //----------------------------------------------------------------------------
 
-WolveSearch::WolveSearch()
-    : SgSearch(0),
-      //m_varTT(16),           // 16bit variation trans-table
-      m_backup_ice_info(true)
-{
-    m_plyWidth.push_back(20);
-    m_plyWidth.push_back(20);
-    m_plyWidth.push_back(20);
-    m_plyWidth.push_back(20);
-}
-
-WolveSearch::~WolveSearch()
-{
-}
-
-bool WolveSearch::CheckDepthLimitReached() const
-{
-    return true;
-}
-
-std::string WolveSearch::MoveString(SgMove move) const
-{
-    if (move == SG_NULLMOVE)
-        return "null";
-    return HexPointUtil::ToString(static_cast<HexPoint>(move));
-}
-
-void WolveSearch::OnStartSearch()
-{
-    //m_varTT.Clear();  // *MUST* clear old variation TT!
-    m_sequence.clear();
-    m_consider.clear();
-}
-
-int WolveSearch::Evaluate(bool* isExact, int depth)
-{
-    UNUSED(depth);
-    int sgScore;
-    if (EndgameUtils::IsDeterminedState(*m_brd, m_toPlay))
-    {
-        *isExact = true;
-        sgScore = EndgameUtils::IsWonGame(*m_brd, m_toPlay) 
-            ? +SgSearchValue::MAX_VALUE
-            : -SgSearchValue::MAX_VALUE;
-    }
-    else 
-    {
-        Resistance resist;
-        ComputeResistance(resist);
-        HexEval score = (m_toPlay == BLACK) ? resist.Score() : -resist.Score();
-        sgScore = ConvertToSgScore(score);
-    }
-    return sgScore;
-}
-
-bool WolveSearch::EndOfGame() const
-{
-    return EndgameUtils::IsDeterminedState(*m_brd, m_toPlay);
-}
-
-void WolveSearch::Generate(SgVector<SgMove>* moves, int depthRemaining)
-{
-    UNUSED(depthRemaining);
-    if (!EndgameUtils::IsDeterminedState(*m_brd, m_toPlay))
-    {
-        bitset_t consider;
-        VariationInfo varInfo;
-#if 0
-        if (m_varTT.Get(SequenceHash::Hash(m_sequence), varInfo))
-        {
-            LogFine() << "Using consider set from TT.\n"
-                      << HexPointUtil::ToString(m_sequence) << '\n'
-                      << m_brd << '\n';
-            consider = varInfo.consider;
-        }
-#endif
-        consider = EndgameUtils::MovesToConsider(*m_brd, m_toPlay);
-        m_consider.push_back(consider);
-        Resistance resist;
-        ComputeResistance(resist);
-        OrderMoves(consider, resist, *moves);
-    }
-}
-
-bool WolveSearch::Execute(SgMove move, int* delta, int depth)
-{
-    UNUSED(delta);
-    UNUSED(depth);
-    m_brd->PlayMove(m_toPlay, static_cast<HexPoint>(move));
-    m_toPlay = !m_toPlay;
-    return true;
-}
-
-void WolveSearch::StartOfDepth(int depth)
-{
-    LogInfo() << "===== Depth " << depth << " =====\n";
-}
-
-void WolveSearch::TakeBack()
-{
-    // TODO: Add AfterStateSearched() functionality here
-    m_brd->UndoMove();
-    m_toPlay = !m_toPlay;
-}
-
-#if 0
-void WolveSearch::AfterStateSearched()
-{
-    if (m_backup_ice_info) 
-    {
-        // Store new consider set in varTT
-        bitset_t old_consider = m_consider[m_current_depth];
-        bitset_t new_consider 
-            = EndgameUtils::MovesToConsider(*m_brd, m_toplay) & old_consider;
-        SgHashCode hash = SequenceHash::Hash(m_sequence);
-        m_varTT.Put(hash, VariationInfo(m_current_depth, new_consider));
-    }
-    m_consider.pop_back();
-}
-#endif
-
-/** Computes resistance on game board using vcs from filled-in
-    board. */
-void WolveSearch::ComputeResistance(Resistance& resist)
-{
-    StoneBoard plain(m_brd->Width(), m_brd->Height());
-    plain.SetPosition(m_brd->GetPosition());
-    Groups groups;
-    GroupBuilder::Build(plain, groups);
-    AdjacencyGraph graphs[BLACK_AND_WHITE];
-    ResistanceUtil::AddAdjacencies(*m_brd, graphs);
-    resist.Evaluate(groups, graphs);
-}
-
-/** Sorts moves in consider set and stores the best in outMoves. The
-    number of moves copied is equal to plyWidth[CurrentDepth()]. */
-void WolveSearch::OrderMoves(const bitset_t& consider,
-                             const Resistance& resist,
-                             SgVector<SgMove>& outMoves) const
-{
-    std::vector<std::pair<HexEval, HexPoint> > mvsc;
-    for (BitsetIterator it(consider); it; ++it) 
-    {
-        HexEval score = resist.Score(*it);
-        mvsc.push_back(std::make_pair(-score, *it));
-    }
-    // NOTE: To ensure we are deterministic, we must use stable_sort.
-    stable_sort(mvsc.begin(), mvsc.end());
-    const std::size_t movesToCopy 
-        = std::min(mvsc.size(), m_plyWidth[CurrentDepth()]);
-    for (std::size_t i = 0; i < movesToCopy;  ++i)
-        outMoves.PushBack(mvsc[i].second);
-}
-
-//----------------------------------------------------------------------------
---
+++ src/wolve/WolveSearch.hpp
@@ -0,0 +1,232 @@
+//----------------------------------------------------------------------------
+/** @file WolveSearch.hpp */
+//----------------------------------------------------------------------------
+
+#ifndef WOLVESEARCH_HPP
+#define WOLVESEARCH_HPP
+
+#include "SgSearch.h"
+#include "SgHashTable.h"
+#include "HexSgUtil.hpp"
+#include "Resistance.hpp"
+
+_BEGIN_BENZENE_NAMESPACE_
+
+//----------------------------------------------------------------------------
+
+/** Varaition TT entry. */
+struct VariationInfo
+{
+    /** Depth state was searched. */
+    int m_depth;
+
+    /** Moves to consider from this variation. */
+    bitset_t m_consider;
+
+    VariationInfo();
+    
+    VariationInfo(int depth, const bitset_t& consider);
+
+    ~VariationInfo();
+    
+    bool IsValid() const;
+
+    void Invalidate();
+
+    bool IsBetterThan(const VariationInfo& other) const;
+
+private:
+    bool m_isValid;    
+};
+
+inline VariationInfo::VariationInfo()
+    : m_isValid(false)
+{
+}
+
+inline VariationInfo::VariationInfo(int depth, const bitset_t& consider)
+    : m_depth(depth),
+      m_consider(consider),
+      m_isValid(false)
+{
+}
+
+inline VariationInfo::~VariationInfo()
+{
+}
+
+inline bool VariationInfo::IsValid() const
+{
+    return m_isValid;
+}
+
+inline void VariationInfo::Invalidate()
+{
+    m_isValid = false;
+}
+
+inline bool VariationInfo::IsBetterThan(const VariationInfo& other) const
+{
+    return m_depth > other.m_depth;
+}
+
+//-------------------------------------------------------------------------- 
+
+/** Performs an AlphaBeta search using Resistance for evaluations. */
+class WolveSearch : public SgSearch
+{
+public:
+    WolveSearch();
+
+    virtual ~WolveSearch();
+
+    //-----------------------------------------------------------------------
+
+    /** Moves to consider in the root state -- this set is used
+        instead of the GenerateMoves() since it could have more
+        knowledge. */
+    bitset_t RootMovesToConsider() const;
+
+    /** See RootMovesToConsider() */
+    void SetRootMovesToConsider(const bitset_t& consider);
+
+    /** Number of moves to consider at each depth after move
+        ordering. */
+    const std::vector<std::size_t>& PlyWidth() const;
+
+    /** See PlyWidth() */
+    void SetPlyWidth(const std::vector<std::size_t>& width);
+
+    void SetWorkBoard(HexBoard* brd);
+
+    //-----------------------------------------------------------------------
+
+    /** @name Virtual methods from HexAbSearch. */
+    // @{
+
+    bool CheckDepthLimitReached() const;
+
+    std::string MoveString(SgMove move) const;
+
+    void SetToPlay(SgBlackWhite toPlay);
+
+    SgBlackWhite GetToPlay() const;
+
+    void StartOfDepth(int depthLimit);
+
+    void OnStartSearch();
+
+    void Generate(SgVector<SgMove>* moves, int depth);
+
+    int Evaluate(bool* isExact, int depth);
+
+    bool Execute(SgMove move, int* delta, int depth);
+
+    void TakeBack();
+
+    SgHashCode GetHashCode() const;
+
+    bool EndOfGame() const;
+
+    // @}
+
+    //-----------------------------------------------------------------------
+
+    /** @name Parameters */
+    // @{
+
+    /** Whether the backed-up ice info is used to reduce the moves to
+        consider after a state has been searched. This is useful if
+        using iterative deepening, since the next time the state is
+        visited a smaller number of moves need to be considered. */
+    bool BackupIceInfo() const;
+
+    /** BackupIceInfo() */
+    void SetBackupIceInfo(bool enable);
+
+    // @}
+
+private:
+    HexBoard* m_brd;
+
+    /** Consider sets for each depth. */
+    std::vector<bitset_t> m_consider;
+
+    /** Sequence of moves from the root. */
+    MoveSequence m_sequence;
+
+    /** See PlyWidths() */
+    std::vector<std::size_t> m_plyWidth;
+
+    /** See RootMovesToConsider() */
+    bitset_t m_rootMTC;
+
+    /** Variation TT. */
+    //TransTable<VariationInfo> m_varTT;
+
+    HexColor m_toPlay;
+
+    /** See BackupIceInfo() */
+    bool m_backup_ice_info;
+
+    void ComputeResistance(Resistance& resist);
+
+    void OrderMoves(const bitset_t& consider, const Resistance& resist,
+                    SgVector<SgMove>& outMoves) const;
+};
+
+inline bitset_t WolveSearch::RootMovesToConsider() const
+{
+    return m_rootMTC;
+}
+
+inline void WolveSearch::SetRootMovesToConsider(const bitset_t& consider)
+{
+    m_rootMTC = consider;
+}
+
+inline const std::vector<std::size_t>& WolveSearch::PlyWidth() const
+{
+    return m_plyWidth;
+}
+
+inline void WolveSearch::SetPlyWidth(const std::vector<std::size_t>& width)
+{
+    m_plyWidth = width;
+}
+
+inline bool WolveSearch::BackupIceInfo() const
+{
+    return m_backup_ice_info;
+}
+
+inline void WolveSearch::SetBackupIceInfo(bool enable)
+{
+    m_backup_ice_info = enable;
+}
+
+inline void WolveSearch::SetWorkBoard(HexBoard* brd)
+{
+    m_brd = brd;
+}
+
+inline void WolveSearch::SetToPlay(SgBlackWhite toPlay)
+{
+    m_toPlay = HexSgUtil::SgColorToHexColor(toPlay);
+}
+
+inline SgBlackWhite WolveSearch::GetToPlay() const
+{
+    return HexSgUtil::HexColorToSgColor(m_toPlay);
+}
+
+inline SgHashCode WolveSearch::GetHashCode() const
+{
+    return m_brd->GetPosition().Hash();
+}
+
+//----------------------------------------------------------------------------
+
+_END_BENZENE_NAMESPACE_
+
+#endif // WOLVESEARCH_HPP
