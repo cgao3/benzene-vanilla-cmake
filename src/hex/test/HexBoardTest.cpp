6e75bfa5937012762c732b0a9026a9cab4840a39
Jakub Pawlewicz <pan@mimuw.edu.pl>
2012-02-03 21:42:01 -0700

    Fixed test cases to work with the new vcs interface

--- src/hex/test/HexBoardTest.cpp
+++ src/hex/test/HexBoardTest.cpp
@@ -6,7 +6,7 @@
 #include <boost/test/auto_unit_test.hpp>
 
 #include "HexBoard.hpp"
-#include "VCSet.hpp"
+#include "VCS.hpp"
 
 using namespace benzene;
 
@@ -22,15 +22,15 @@ BOOST_AUTO_TEST_CASE(HexBoard_PlayAndUndo)
     VCBuilderParam param;
     HexBoard brd(7, 7, ice, param);
     brd.ComputeAll(BLACK);
-    BOOST_CHECK(!brd.Cons(BLACK).Exists(NORTH, HEX_CELL_A4, VC::FULL));
+    BOOST_CHECK(!brd.Cons(BLACK).FullExists(NORTH, HEX_CELL_A4));
 
     brd.PlayMove(BLACK, HEX_CELL_B2);
     BOOST_CHECK_EQUAL(brd.GetPosition().GetColor(HEX_CELL_B2), BLACK);
-    BOOST_CHECK(brd.Cons(BLACK).Exists(NORTH, HEX_CELL_A4, VC::FULL));
+    BOOST_CHECK(brd.Cons(BLACK).FullExists(NORTH, HEX_CELL_A4));
 
     brd.UndoMove();
     BOOST_CHECK(brd.GetPosition().IsEmpty(HEX_CELL_B2));
-    BOOST_CHECK(!brd.Cons(BLACK).Exists(NORTH, HEX_CELL_A4, VC::FULL));
+    BOOST_CHECK(!brd.Cons(BLACK).FullExists(NORTH, HEX_CELL_A4));
 }
 
 BOOST_AUTO_TEST_CASE(HexBoard_CopyConstructor)
@@ -41,15 +41,13 @@ BOOST_AUTO_TEST_CASE(HexBoard_CopyConstructor)
     brd.ComputeAll(BLACK);
     brd.PlayMove(BLACK, HEX_CELL_B2);
     BOOST_CHECK_EQUAL(brd.GetPosition().GetColor(HEX_CELL_B2), BLACK);
-    BOOST_CHECK(brd.Cons(BLACK).Exists(NORTH, HEX_CELL_A4, VC::FULL));
+    BOOST_CHECK(brd.Cons(BLACK).FullExists(NORTH, HEX_CELL_A4));
 
     HexBoard cpy(brd);
     BOOST_CHECK_EQUAL(cpy.GetPosition().GetColor(HEX_CELL_B2), BLACK);
-    BOOST_CHECK(cpy.Cons(BLACK).Exists(NORTH, HEX_CELL_A4, VC::FULL));
 
     brd.UndoMove();
     BOOST_CHECK_EQUAL(cpy.GetPosition().GetColor(HEX_CELL_B2), BLACK);
-    BOOST_CHECK(cpy.Cons(BLACK).Exists(NORTH, HEX_CELL_A4, VC::FULL));    
 }
 
 //---------------------------------------------------------------------------
