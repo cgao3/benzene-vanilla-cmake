313bbd6afd669b9e06aefb344916e3f7052c0e13
Chao Gao <cgao3@ualberta.ca>
2019-12-03 16:11:46 -0700

    standalone JY program refined by David

--- src/jingyang/main.cxx
+++ src/jingyang/main.cxx
@@ -6,12 +6,16 @@
 #include<unordered_map>
 #include<queue>
 #include<set>
+#include<map>
+#include<stack>
 #include<cassert>
 #include<algorithm>
 
-#define BOARDSIZE 9
+int BOARDSIZE = 9;
 
 using namespace std;
+
+
 
 
 string strip(string &line){
@@ -54,6 +58,19 @@ string point_to_cell(int point){
     return ret;
 }
 
+string point_to_benzene_cell(int point){
+    //in benzene local point 1 refers to a1 but in this program local 1 refers to b1.
+    //This functions outputs a cell normalized to benzene
+    return point_to_cell(point - 1);
+}
+
+bool empty(vector<string>&board, int point){
+    int x,y;
+    x=point%BOARDSIZE;
+    y=point/BOARDSIZE;   
+    return board[y][x] == '.';
+}
+
 struct PMOVE {
     int local_move; //[1, ]
     int global_move;//in the original board, i.e., in range [0, boardsize**2)
@@ -72,6 +89,121 @@ struct Pattern {
     vector< vector<int> > vc_PSs;//for each branch, list of decomposed pattern IDs
     vector< vector< vector<int> > > vc_vc_PPs; //for each branch, for each decomposed pattern, moves to update
 };
+
+/*
+ * print the current pattern list, which always covers the whole board
+ * regions out of coveraged of those pattern list are not of interest, since
+ * if white plays there, a random counter move can be responded by black
+ */
+std::string PrintCurrentPatternList(vector<string>&board, vector<Pattern>&working_patterns,bool reflect)
+{
+
+    //benzene local is 1 greater than main.cxx local
+    std::ostringstream os;
+    Pattern cur_pattern = working_patterns[working_patterns.size() - 1];
+    if(cur_pattern.RN==1 && BOARDSIZE == 9){
+        //since RN=1 contains only half empty cells on board
+        int mid_point=(BOARDSIZE * BOARDSIZE)/2 ;
+        for(int i=0;i<BOARDSIZE*BOARDSIZE;i++){
+            if(i==mid_point) continue;//center is occupied by first black move
+            int local= (i > mid_point) ? i-1 : i;
+            string hexpoint = point_to_cell((i > mid_point) ? local + 1 : local);
+            os <<' '<<hexpoint<<' '<<local + 1<<"@1";
+        }
+        return os.str();
+    }
+
+    
+    Pattern pattern;
+    for(int index=0;index<working_patterns.size(); index++) {
+        pattern=working_patterns[index];
+        for(int i=0;i<pattern.vc_WMs.size();i++){
+            for(int j=1;j<pattern.vc_WMs[i].size();j++){
+                int global = pattern.vc_WMs[i][j].global_move;
+                int local = pattern.vc_WMs[i][j].local_move;
+                string hexPoint= point_to_cell((reflect) ? reflect_point(global) : global) ;
+                os << ' '<<hexPoint<<' '<<local<<'@'<<pattern.RN;
+            }
+        }
+    }
+
+    return os.str();
+}
+
+std::string PrintCurrentBranchList(vector<string>&board, vector<Pattern>&working_patterns,bool reflect)
+{
+    std::ostringstream os;
+    Pattern pattern;
+    for(int index = 0; index < working_patterns.size(); index++){
+        pattern = working_patterns[index];
+        for(int i=0;i<pattern.BT;i++){
+            for(int j=1;j<pattern.vc_WMs[i].size();j++){
+                int global = pattern.vc_WMs[i][j].global_move;
+                int local = pattern.vc_WMs[i][j].local_move;
+                string hexPoint= point_to_cell((reflect) ? reflect_point(global) : global) ;
+                os << ' '<<hexPoint<<' ' <<pattern.RN << "@" <<pattern.vc_BN[i];
+                if (pattern.RN == 1 && BOARDSIZE == 9){
+                    hexPoint= point_to_cell((!reflect) ? reflect_point(global) : global) ;
+                    os << ' '<<hexPoint<<' ' <<pattern.RN << "@" <<pattern.vc_BN[i];
+                }
+            }
+        }
+    }
+
+    return os.str();
+}
+
+
+std::string PrintCurrentBlackMoves(vector<string>&board, vector<Pattern>&working_patterns,bool reflect)
+{
+    //Create mapping from black hexpoint to all white hexpoints s.t. the black hexpoint is a winning response to all white hexpoints.
+
+    //First create the mapping to ensure it can be printed in order without repititions   
+    std::map<int,std::vector<int>> bmToWM;
+    std::ostringstream os;
+    Pattern pattern;
+    for(int index = 0; index < working_patterns.size(); index++){
+        pattern = working_patterns[index];
+        for(int i=0;i<pattern.BT;i++){
+            for(int j=1;j<pattern.vc_WMs[i].size();j++){
+                if(bmToWM.count(pattern.vc_BM[i].global_move) == 0){
+                    bmToWM.insert(std::pair<int,std::vector<int>>(pattern.vc_BM[i].global_move, {pattern.vc_WMs[i][j].global_move}));
+                }
+                else{
+                    bmToWM.at(pattern.vc_BM[i].global_move).push_back(pattern.vc_WMs[i][j].global_move);
+                }
+            }
+        }
+    }
+    
+    //Format is = bm wm,...,wm .... bm wm,....,wm
+
+    for(auto const &kvp: bmToWM){
+        string hexPoint= point_to_cell((reflect) ? reflect_point(kvp.first) : kvp.first) ;
+
+        os << hexPoint;
+
+        for(int i = 0; i < kvp.second.size(); i++){
+            string hexPoint= point_to_cell((reflect) ? reflect_point(kvp.second[i]) : kvp.second[i]) ;
+            os << ((i == 0) ? ' ' : ',') << hexPoint;
+        }
+
+        os << ' ';
+        //Handle first move case
+        if (working_patterns[working_patterns.size() - 1].RN == 1 && BOARDSIZE == 9){
+            hexPoint=point_to_cell((!reflect) ? reflect_point(kvp.first) : kvp.first) ;
+            os << hexPoint;
+            for(int i = 0; i < kvp.second.size(); i++){
+                string hexPoint = point_to_cell((!reflect) ? reflect_point(kvp.second[i]) : kvp.second[i]) ;                
+                os << ((i == 0) ? ' ' : ',') << hexPoint;
+            }
+            os << ' ';
+        }
+    }
+
+    return os.str();
+}
+
 
 void next(vector<string> &vc_str, int &idx, vector<string> &tokens){
     tokens=split(vc_str[idx], ' ');
@@ -242,8 +374,8 @@ string genmove(string white_move, vector<Pattern>& working_patterns, unordered_m
     //white move didn't find, so play a move in the working_patterns
     cerr<<"no matching white move, select the last pattern "<<pattern.RN<<"\n";
     BM=pattern.vc_BM[0].global_move;
-    add_new_patterns(all_patterns, pattern, 0, working_patterns);
     working_patterns.pop_back();
+    add_new_patterns(all_patterns, pattern,0, working_patterns);
     black_move=point_to_cell(BM);
     return black_move;
 }
@@ -287,6 +419,8 @@ void showboard(const vector<string> &board){
     }
 }
 
+
+
 void play(vector<string> &board, char color, string move){
     int x=move[0]-'a';
     int y=move[1]-'1';
@@ -295,28 +429,38 @@ void play(vector<string> &board, char color, string move){
 
 void gtp_loop(vector<string>&board, unordered_map<int, Pattern> &all_patterns){
     string text;
-    play(board, 'b', "e5");
+    int mid_point = ((BOARDSIZE - 1) / 2 ) * BOARDSIZE + (BOARDSIZE / 2);
+
+    play(board, 'b', point_to_cell(mid_point));
     int toplay=1;
     string white_move="";
     vector<Pattern> working_patterns;
+    stack<vector<Pattern>> previous_working_patterns; 
     Pattern p = all_patterns[1];
     working_patterns.push_back(p);
-    set<int> history;
+    stack<int> history;
     bool reflect=false;
+    bool can_reflect= (BOARDSIZE == 9);
     int point;
     while(true){
         getline(cin, text);
         transform(text.begin(), text.end(), text.begin(), ::tolower);
         if(text.find("quit")!=string::npos) break;
         if(text.find("genmove b")!=string::npos || text.find("genmove black")!=string::npos){
+            if(working_patterns.size() == 0){
+                cout << "= invalid\n" << endl;
+                continue;
+            }
+
             vector<string> tokens=split(text, ' ');
-            string black_move=genmove(white_move, working_patterns, all_patterns);
+            previous_working_patterns.push(working_patterns);
+            string black_move=genmove(white_move, working_patterns, all_patterns);            
             point=cell_to_point(black_move);
-            if(reflect){
+            history.push(point);
+            if(reflect && can_reflect){
                 point = reflect_point(point);
                 black_move=point_to_cell(point);
-            }
-            history.insert(point);
+            }            
             play(board, 'b', black_move);
             toplay=1-toplay;
             cerr<<"Reflect:"<<reflect<<" Patterns now:";
@@ -340,21 +484,24 @@ void gtp_loop(vector<string>&board, unordered_map<int, Pattern> &all_patterns){
                 cerr<<"move out of range\n";
                 continue;
             }
-            if(history.find(point)!=history.end()){
+            cerr << "white move: " << point << "\n" << endl;
+            if(!empty(board,point)){
                 cerr<<"occupied cell\n";
                 continue;
             }
             play(board, tokens[1][0], white_move);
             if(history.size() == 0 && point>BOARDSIZE*BOARDSIZE/2){
-                reflect=true;
+                cerr << "reflecting\n";
+                reflect=true && can_reflect;
             }
+            
             if(reflect){
                 point=reflect_point(point);//symmetric move
                 white_move=point_to_cell(point);
             }
-            history.insert(point);
+            history.push(point);
             toplay=1-toplay;
-            cout<<"=\n"<<endl;
+            cout<<"= \n"<<endl;
         }
         if(text.find("showboard")!=string::npos){
             cout<<"= \n"<<endl;
@@ -375,20 +522,92 @@ void gtp_loop(vector<string>&board, unordered_map<int, Pattern> &all_patterns){
         if(text.find("boardsize")!=string::npos){
             cout<<"= \n"<<endl;
         }
+        if(text.find("show_jypattern_list")!=string::npos){
+            if(working_patterns.size() == 0){
+                cout << "= \n" << endl;
+                continue;
+            }
+            std::ostringstream os;
+            os << "= " << PrintCurrentPatternList(board,working_patterns,reflect) << "\n"; 
+            string out = os.str();
+            cout << out << endl;
+        }
+        if(text.find("show_jybranch_list") != string::npos){
+            if(working_patterns.size() == 0){
+                cout << "= \n" << endl;
+                continue;
+            }
+            cout << "= " << PrintCurrentBranchList(board,working_patterns,reflect) << "\n" << endl; 
+        }
+        if(text.find("show_jyblackmoves_list") != string::npos){
+            if(working_patterns.size() == 0){
+                cout << "= \n" << endl;
+                continue;
+            }
+            cout << "= " << PrintCurrentBlackMoves(board,working_patterns,reflect) << "\n" << endl; 
+        }
+        if(text.find("clear_board") != string::npos){
+            for(int i=0;i<BOARDSIZE;i++){
+                for(int j=0;j<BOARDSIZE;j++){
+                    board[i][j] = '.';
+                }
+            }            
+            white_move="";
+            working_patterns.clear();
+            while(!previous_working_patterns.empty()){
+                previous_working_patterns.pop();
+            }
+            Pattern p = all_patterns[1];
+            working_patterns.push_back(p);
+            while(!history.empty()){
+                history.pop();
+            }
+            reflect=false;
+            point = 0;
+            play(board, 'b', point_to_cell(mid_point));
+            toplay = 1;
+            cout << "= \n" << endl;
+        }
+        if(text.find("undo") != string::npos){
+            if(history.size() > 0){
+                if(toplay==1){
+                    working_patterns = previous_working_patterns.top();
+                    previous_working_patterns.pop();
+                    toplay = 0;
+                }
+                else if(toplay == 0){
+                    toplay = 1;
+                }
+                string lastmove = point_to_cell((reflect) ? reflect_point(history.top()) : history.top());
+                cerr << "Undoing: " << lastmove << "\n" << endl;
+                play(board,'.',lastmove);
+                history.pop();
+                cerr << "size: " << history.size() << "\n" << endl;
+                if(history.size() == 0 && reflect){
+                    cerr << "Unreflecting\n" << endl;
+                    reflect = false;
+                }
+            }
+            cout << "= \n" << endl;
+        }
     }
 }
 
 int main(int argc, char *argv[]){
     if (argc<2){
-        cout<<"usage: ./program jingyang_pattern.txt"<<endl;
+        cout<<"usage: ./program jingyang_pattern.txt board_dim"<<endl;
         return 1;
     }
     string file_name=string(argv[1]);
-    cerr<<"pattern file:"<<file_name<<endl;
     string line;
     ifstream infile(file_name.c_str());
     vector<string> vc_str;
     while(getline(infile, line)){
+        if (line[0] == '#'){
+            BOARDSIZE = stoi(split(line,' ')[1]);
+            cerr << "Found boardsize:" << BOARDSIZE << "\n" << endl;
+        }
+
         if (line.size()==0 || line[0]!='"')
             continue;
         vc_str.push_back(strip(line));
